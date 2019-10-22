eefdcd43ab8b06f2a659839e1bdee6bb5f2dfdae
Wallace Frota <frotadev@gmail.com>
2019-10-22 10:36:21 -0300

    Alterado o arquivo principal app.js

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+// DEVELOPED BY - WALLACE FROTA
 // requerendo modulos
 const express = require('express');
 const app = express(); // todas as funções do express
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const session = require('express-session')
+const flash = require("req-flash");
 const path = require('path');
 
 const Post = require('./models/Post'); // tabela do bd
 
 // config
+  // sessão armazenar req flash
+  app.use(session ({
+      secret: "crudnode",
+      resave: true,
+      saveUninitialized: true
+  }))
+  // req-flash
+  app.use(flash());
+  // middleware flash
+  app.use((req, res, next) => {
+      res.locals.success_msg = req.flash("success_msg");
+      res.locals.error_msg = req.flash("error_msg");
+      next()
+  })
   // handlebars
   app.engine('handlebars', handlebars({defaultLayout: 'main'})); // main arquivo principal, estrutura HTML
   app.set('view engine', 'handlebars');
@@ -15,11 +32,10 @@ const Post = require('./models/Post'); // tabela do bd
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
 
-  //Path arquivos estaticos css/bootstrap/jquery/img
-  app.use(express.static(path.join(__dirname, "public")));
+  //Public Path arquivos estaticos css/bootstrap/jquery/img
+  app.use(express.static(path.join(__dirname, "/public")));
 
 // ROTAS
-
 // cadastro exibindo formulário
 app.get("/cad", (req, res) => {
     res.render('form');
@@ -34,19 +50,46 @@ app.get('/', (req, res) => {
 
 // rota de administração das postagens
 app.get('/admin', (req, res) => {
-    Post.findAll({order: [['id', 'DESC']]}).then((postagens) => {
-        res.render('viewpostadm', {postagens: postagens})
+    Post.findAll({order: [['id', 'DESC']]}).then((posts) => {
+        res.render('viewpostadm', {posts: posts})
     })
 })
 
 // rota excluir postagens via admin pelo id da postagem
 app.get('/deletar/:id', (req, res) => {
     Post.destroy({where: {'id': req.params.id}}).then(() => {
+        req.flash("success_msg", "Postagem deletada com sucesso.")
         res.redirect('/admin')
     }).catch((error) => {
-        res.send("Erro ao deletar postagem: " + error)
+        req.flash("error_msg", "Erro ao excluir postagem.")
+        res.redirect('/admin')
     })
 })
+
+// rota do form de update de dados com os valores sendo exibidos ao admin
+app.get('/update/:id', (req, res) => {
+    Post.findOne({where: {'id': req.params.id}}).then((dados) => {
+        res.render('update', {id: req.params.id, dados: dados});
+    })
+})
+
+// rota atualizando dados das postagens via admin
+app.post('/addupdate', (req, res) => {
+    Post.findOne({where: {'id': req.body.id}}).then((dados) => {
+        dados.author = req.body.author
+        dados.titulo = req.body.titulo
+        dados.frase = req.body.frase
+        // se foi salvo redireciona
+        dados.save().then(() => {
+            req.flash("success_msg", "Dados atualizados com sucesso.")
+            res.redirect('/admin')
+        })
+    }).catch((error) => {
+        req.flash("error_msg", "Erro ao atualizar informações.");
+        res.redirect('/admin')
+    })
+})
+
 
  // rota post salvando no bd
 app.post('/add', (req, res) => {
