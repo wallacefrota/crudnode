551d51ccc0d44012d1b04c7efe6a77a9e8538061
Wallace Frota <frotadev@gmail.com>
2020-06-09 10:25:31 -0300

    Alterado caminho views e adicionado arquivos de rotas

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const bodyParser = require('body-parser');
 const session = require('express-session')
 const flash = require("req-flash");
 const path = require('path');
-
-const Post = require('./models/Post'); // tabela do bd
+// rotas
+const routes = require('./src/routes');
 
 // config
   // sessão armazenar req flash
@@ -27,8 +27,8 @@ const Post = require('./models/Post'); // tabela do bd
       next()
   })
   // handlebars
-  app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/'}));
-  app.set('views', path.join(__dirname, 'views'));
+  app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/src/views/layouts/'}));
+  app.set('views', path.join(__dirname, 'src', 'views'));
   app.set('view engine', 'hbs');
   
   // body parser
@@ -38,76 +38,8 @@ const Post = require('./models/Post'); // tabela do bd
   //Public Path arquivos estaticos css/bootstrap/jquery/img
   app.use(express.static(path.join(__dirname, "/public")));
 
-// ROTAS
-// cadastro exibindo formulário
-app.get("/cad", (req, res) => {
-    res.render('form');
-})
-
-// rota principal exibindo postagens em ordem desc
-app.get('/', (req, res) => {
-     Post.findAll({order: [['id', 'DESC']]}).then((posts) => {
-        res.render('home', {posts: posts});
-     })
-})
-
-// rota de administração das postagens
-app.get('/admin', (req, res) => {
-    Post.findAll({order: [['id', 'DESC']]}).then((posts) => {
-        res.render('viewpostadm', {posts: posts})
-    })
-})
-
-// rota excluir postagens via admin pelo id da postagem
-app.get('/deletar/:id', (req, res) => {
-    Post.destroy({where: {'id': req.params.id}}).then(() => {
-        req.flash("success_msg", "Postagem deletada com sucesso.")
-        res.redirect('/admin')
-    }).catch((error) => {
-        req.flash("error_msg", "Erro ao excluir postagem.")
-        res.redirect('/admin')
-    })
-})
-
-// rota do form de update de dados com os valores sendo exibidos ao admin
-app.get('/update/:id', (req, res) => {
-    Post.findOne({where: {'id': req.params.id}}).then((dados) => {
-        res.render('update', {dados: dados});
-    })
-})
-
-// rota atualizando dados das postagens via admin
-app.post('/addupdate', (req, res) => {
-    Post.findOne({where: {'id': req.body.id}}).then((dados) => {
-        dados.author = req.body.author
-        dados.titulo = req.body.titulo
-        dados.frase = req.body.frase
-        // se foi salvo redireciona
-        dados.save().then(() => {
-            req.flash("success_msg", "Dados atualizados com sucesso.")
-            res.redirect('/admin')
-        })
-    }).catch((error) => {
-        req.flash("error_msg", "Erro ao atualizar informações.");
-        res.redirect('/admin')
-    })
-})
-
-
- // rota post salvando no bd
-app.post('/add', (req, res) => {
-    // salvando os dados dos inputs no bd com bodyParser
-    Post.create({
-        author: req.body.author,
-        titulo: req.body.titulo,
-        frase: req.body.frase
-    }).then(function() { // se cadastrou com sucesso redireciona
-        res.redirect('/')
-    }).catch(function(error) { // se houve erro
-        res.send("Ocorreu um erro ao salvar dados: " + error)
-    })
-})
-
+  // rotas
+  app.use('/', routes);
 
 // acesso ao servidor porta local
 app.listen('8081', function() {
